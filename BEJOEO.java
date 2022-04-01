a092513b073cae19c9b30d188c1b25950e7457d1
Austin Lu <austinlu@Anjians-MacBook-Pro.local>
2022-03-31 21:58:23 -0400

    initial commit

---
+++ BEJOEO.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class BEJOEO {
+	public static void main(String[] args) {
+
+
+		/*System.out.println("Wat ur nam");
+		Scanner sc = new Scanner(System.in);
+		String name = String.format("Name: %s\n", sc.nextLine());
+		System.out.println("wat age");
+		String age = String.format("Age: %d\n", sc.nextInt());
+		System.out.println("wat email");
+		String email = String.format("contact: %s\n", sc.nextLine());
+		System.out.println("wat address");
+		String address = String.format("Located at: %s\n", sc.nextLine());
+		System.out.println(name + age + email + address);
+		*/
+		/*Scanner number = new Scanner(System.in);
+		System.out.println("giv operation(+, *, -, /)");
+		String operation = number.nextLine();
+		System.out.println("giv number");
+		double num1 = number.nextDouble();
+		
+		System.out.println("giv number again lol");
+		double num2 = number.nextDouble();
+		
+		switch(operation){
+			case "+":
+				Double result = num1 + num2;
+				System.out.println(result);
+				break;
+			case "-":
+				Double result2 = num1 - num2;
+				System.out.println(result2);
+				break;
+			case "*":
+				Double result3 = num1 * num2;
+				System.out.println(result3);
+				break;
+			case "/":
+				Double result4 = num1 + num2;
+				System.out.println(result4);
+				break;
+			default:
+				System.out.println("invalid");
+		}*/
+
+		for(x = 0; x > 1; x++){
+			
+		}
+		
+	}
+}
